f27b39045422f7bf1f3cde73417ed82764e63260
FeSchneider7 <fernandomoraes26@gmail.com>
2025-02-18 14:17:41 -0300

    atualizaçãoAula

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import http from "http";
 
 const server = http.createServer((req, res) => {
-    return res.end('Hello World')
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World - Schneider no seu Browser');
 });
 
 server.listen(3000, () => {
-    console.log('Server is running on port 3000')
+    console.log('✅ Server is running on port 3000 no seu Browser');
 });
-

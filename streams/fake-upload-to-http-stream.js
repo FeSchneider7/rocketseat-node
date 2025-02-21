fcef5fbbb3b0eabd00e663d2973ea1c933a3c56a
FeSchneider7 <fernandomoraes26@gmail.com>
2025-02-21 14:56:53 -0300

    atualizacaoAula

---
+++ streams/fake-upload-to-http-stream.js
@@ -0,0 +1,27 @@
+import { Readable } from 'node:stream';
+
+class OneToHundredStream extends Readable {
+    index = 1;
+
+    _read() { 
+        const i = this.index++;
+
+        setTimeout(() => {
+            if (i > 100) {
+                this.push(null);
+            } else {
+                this.push(`${i}\n`);
+            }
+        }, 1000);
+    }
+}
+
+fetch('http://localhost:3334', {
+    method: 'POST',
+    body: new OneToHundredStream(),
+    duplex: 'half', // 🔥 Corrige o erro no Node.js 18+
+    headers: { 'Content-Type': 'text/plain' } // Opcional, mas ajuda no servidor
+})
+  .then(response => response.text())
+  .then(console.log)
+  .catch(console.error);

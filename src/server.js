46e7d050f0752936d6cb0d1f0a1447af5e5f3711
FeSchneider7 <fernandomoraes26@gmail.com>
2025-02-23 12:41:36 -0300

    atualizacaoAula

--- src/server.js
+++ src/server.js
@@ -41,10 +41,23 @@ import http from "http";
 
 const users = []
 
-const server = http.createServer((req, res) => {
+const server = http.createServer(async (req, res) => {
 
     const { method, url } = req;
 
+    const buffers = []
+
+    for await (const chunk of req) {
+        buffers.push(chunk)
+    }
+
+    try {
+        req.body = JSON.parse(Buffer.concat(buffers).toString())
+    } catch {
+        req.body = {}
+    }
+
+  
     if (method === "GET" && url === "/users") {
         return res
         .setHeader('Content-Type', 'application/json')
@@ -53,10 +66,13 @@ const server = http.createServer((req, res) => {
 
 
     if (method === "POST" && url === "/users") {
+        const { name, email} = req.body;
+    
+
         users.push({
             id: 1,
-            name: 'Lucas',
-            email: 'lucasnenem@gmail.com',
+            name,
+            email,
         })
 
         return res.writeHead(201) .end();

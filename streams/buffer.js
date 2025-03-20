50a369aa0c64b4e5e9beb20f44dad333747f05dc
Fernando Schneider <fernandomoraes26@gmail.com>
2025-03-20 17:50:56 -0300

    aula

---
+++ streams/buffer.js
@@ -0,0 +1,7 @@
+//buffer é uma representação de espaço de memória no computador
+//buffer é um array de bytes
+//buffer é uma forma de representar dados binários
+//buffer é uma forma de representar dados de forma eficiente
+
+const buff = ArrayBuffer.from("Hello World");
+console.log(buff);
